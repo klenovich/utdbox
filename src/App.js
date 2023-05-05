df203781f4cff0f8138c06f1e1e570e26d7c9d6e
klucas12345 <89214539+klucas12345@users.noreply.github.com>
2023-05-05 16:05:33 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,184 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="wrapper">
+        <div id="main">
+            <div class="inner">
+                <div id="container01" class="style2 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <div id="image01" class="style2 image">
+                                <span class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image01.jpg?v=c36759b0" alt=""/>
+                                </span>
+                            </div>
+                            <p id="text17" class="style7">
+                                <span class="p">
+                                    A Greater Cause Foundation
+                                    <br/>
+                                     and Kappa sigma at ut-Dallas
+                                </span>
+                            </p>
+                            <h1 id="text03" class="style4">BOX-A-THON</h1>
+                            <p id="text16" class="style6">Box-A-Thon is a week long event where we raise awareness, support and donations for homeless veterans. Every year we construct a giant box in the center of campus and have two members at the box at all times during the week to put ourselves in the shoes of the homeless veterans we&#039;re supporting.</p>
+                            <ul id="buttons02" class="style3 buttons">
+                                <li>
+                                    <a href="https://www.gofundme.com/boxathonutd" class="button n01">
+                                        <svg>
+                                            <use href="#icon-a3c"></use>
+                                        </svg>
+                                        <span class="label">GOFUNDME LINK</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="container04" data-scroll-id="one" data-scroll-behavior="center" data-scroll-offset="1.5" data-scroll-invisible="1" class="style1 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <p id="text19" class="style5">Box-a-thon 2022</p>
+                            <h2 id="text20" class="style3">September 26th - 30th</h2>
+                            <div id="image06" class="style1 image full" data-position="center">
+                                <span class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image06.jpg?v=c36759b0" alt=""/>
+                                </span>
+                            </div>
+                            <p id="text21" class="style1">This year Box-a-thon will be held at the UT-Dallas plinth from September 26th to the 30th. We will be collecting donations for our Military Heroes Campaign on campus during this period.</p>
+                            <ul id="buttons06" class="style1 buttons">
+                                <li>
+                                    <a href="https://www.gofundme.com/boxathonutd" class="button n01">GOFUNDME</a>
+                                </li>
+                            </ul>
+                            <ul id="buttons07" class="style1 buttons">
+                                <li>
+                                    <a href="#" class="button n01">Venmo @utdkappasig</a>
+                                </li>
+                            </ul>
+                            <ul id="buttons08" class="style1 buttons">
+                                <li>
+                                    <a href="#" class="button n01">CashApp $utdbox</a>
+                                </li>
+                            </ul>
+                            <ul id="buttons03" class="style1 buttons">
+                                <li>
+                                    <a href="#" class="button n01">
+                                        Zelle 
+                                        <span class="__cf_email__" data-cfemail="1a607f76767f5a6f6e7e7875623475687d">[email&#160;protected]</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="container08" data-scroll-id="two" data-scroll-behavior="center" data-scroll-offset="1.5" data-scroll-invisible="1" class="style1 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <h2 id="text10" class="style3">Military Heroes Campaign</h2>
+                            <p id="text07" class="style1">The Kappa Sigma Military Heroes Campaign is committed to supporting ongoing care for our military veterans and their families. Since 2007, the Kappa Sigma Military Heroes Campaign has raised over $2 million dollars to aid our veterans across North America. Since our organization is operated by volunteers and the Kappa Sigma Fraternity covers all overhead, you can be assured that 100% of your generous donations to the Military Heroes Campaign will go to our partners and our veterans.</p>
+                            <div id="image03" class="style1 image full" data-position="center">
+                                <span class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image03.jpg?v=c36759b0" alt=""/>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="container03" data-scroll-id="three" data-scroll-behavior="center" data-scroll-offset="1.5" data-scroll-invisible="1" class="style1 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <h2 id="text09" class="style3">Special Thanks</h2>
+                            <p id="text18" class="style9">Raafeh at LaLaLand Kind Cafe, Campbell Rd for providing $1000+ in pastries to give to donors</p>
+                            <div id="image04" class="style3 image full" data-position="center">
+                                <a href="https://lalalandkindcafe.com/locations/" class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image04.jpg?v=c36759b0" alt=""/>
+                                </a>
+                            </div>
+                            <p id="text14" class="style9">Local DFW Home Depot locations for providing constructon materials</p>
+                            <div id="image05" class="style3 image full" data-position="center">
+                                <a href="https://lalalandkindcafe.com/locations/" class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image05.jpg?v=c36759b0" alt=""/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="container09" class="style1 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <p id="text24" class="style5">Our chapter at utd</p>
+                            <h2 id="text08" class="style3">Kappa Sigma Nu-Omicron</h2>
+                            <p id="text06" class="style1">
+                                <span class="p">The brothers of our organization have accomplished many things over the course of the last 30 years of its reign on this campus. From becoming the first Fraternity chartered here on March 28, 1992, we have now had over 600 brothers come through Nu-Omicron, those of which have been highly successful during their times here at UT Dallas, and beyond. Post graduation our brothers have progressed incredibly, and continue to give back to Kappa Sigma, because in the words of many -</span>
+                                <span class="p">“Kappa Sigma was the best decision of my life”</span>
+                                <span class="p">The values, principles, work ethic, and not to mention lifelong friendships and relationships built during the years of your undergraduate term are those that you will carry forward with you not for a day, a year, or a college term only, but for life.</span>
+                            </p>
+                            <ul id="buttons05" class="style1 buttons">
+                                <li>
+                                    <a href="https://domain.ext/path" class="button n01">Button</a>
+                                </li>
+                            </ul>
+                            <div id="image08" class="style1 image full" data-position="center">
+                                <span class="frame">
+                                    <img src="https://boxathon.carrd.co/assets/images/image08.jpg?v=c36759b0" alt=""/>
+                                </span>
+                            </div>
+                            <ul id="buttons04" class="style1 buttons">
+                                <li>
+                                    <a href="#four" class="button n01">more info</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="container02" data-scroll-id="four" data-scroll-behavior="center" data-scroll-offset="1.5" data-scroll-invisible="1" class="style2 container default">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1">
+                            <p id="text04" class="style7">utdbox.org</p>
+                            <h2 id="text15" class="style8">KSNO</h2>
+                            <p id="text13" class="style6">Social Media</p>
+                            <ul id="icons02" class="style1 icons">
+                                <li>
+                                    <a class="n01" href="https://www.instagram.com/utdkappasig/">
+                                        <svg>
+                                            <use href="#icon-910"></use>
+                                        </svg>
+                                        <span class="label">Instagram</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="n02" href="https://www.facebook.com/UTDallasKappaSigma/">
+                                        <svg>
+                                            <use href="#icon-906"></use>
+                                        </svg>
+                                        <span class="label">Facebook</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="n03" href="/cdn-cgi/l/email-protection#d2b5bfb9a1bcbd92b5bfb3bbbefcb1bdbf">
+                                        <svg>
+                                            <use href="#icon-90a"></use>
+                                        </svg>
+                                        <span class="label">Email</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="credits" class="icc-credits">
+                    <span>
+                        <a href="https://carrd.co/build?ref=auto">Made with Carrd</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
